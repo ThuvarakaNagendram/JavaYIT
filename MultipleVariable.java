bfefa8f305ace0cb24a55617d98046279689b916
ThuvarakaNagendram <111124391+ThuvarakaNagendram@users.noreply.github.com>
2026-02-09 14:51:26 +0530

    Arithmetic and assign operators

---
+++ MultipleVariable.java
@@ -0,0 +1,11 @@
+class MultipleVariable{
+	public static void main (String args[]) {
+		String fName, lName;
+		fName="Yoga";
+		lName="Seelan";
+		System.out.println("First name is "+fName);
+		System.out.println("Last name is "+lName);
+		System.out.println("Fullname is "+ fName + "" + lName);
+	}
+
+}
